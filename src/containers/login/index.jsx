1a85ed0bbe227a5da86a4825635d789b486efe0e
Ronak Kapadi <ronakkapadi22@gmail.com>
2024-09-03 18:50:56 +0530

    FIXED OTP issue

--- src/containers/login/index.jsx
+++ src/containers/login/index.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useEffect } from "react";
 import logo from "../../assets/images/logo.png";
 import Form from "../../shared/form";
 import FormControl from "../../shared/form-control";
@@ -6,49 +6,61 @@ import CustomCheckBox from "../../shared/checkbox";
 import Button from "../../shared/button";
 import { useFormik } from "formik";
 import { LoginSchema } from "../../assets/utils/validation";
-import { RecaptchaVerifier, signInPhoneNumber } from "../../firebase";
+import { RecaptchaVerifier, signInWithPhoneNumber, auth } from "../../firebase";
 
 const Login = () => {
 
-  // Correctly initialize Recaptcha only once
-  const captchaVerifier = (id,handleSubmit) => {
-    window.recaptchaVerifier = new RecaptchaVerifier(id, {
-            'size': 'invisible',
-            'callback': (response) => {
-                // reCAPTCHA solved, allow signInWithPhoneNumber.
-                handleSubmit();
-                console.log(response);
-                console.log('recaptcha verified');
-            },
-            defaultCountry: 'IN'
-        });
-}
-      
-
-    const handleSendOTP = async(values) => {
-        captchaVerifier('sign-in-button', handleSendOTP)
-        const appVerifier = window.recaptchaVerifier;
-        const otpSent = signInPhoneNumber('+91'+values.phoneNumber, appVerifier)
-        console.log('OTP Sent', otpSent)
-    }
-
-    const { values, errors, handleSubmit, setValues } = useFormik({
-        initialValues: {
-            phoneNumber: null,
-            keepSignIn: false
+  
+  const onCaptchVerify = () => {
+    if (!window.recaptchaVerifier) {
+      window.recaptchaVerifier = new RecaptchaVerifier(
+        "recaptcha-container",
+        {
+          size: "invisible",
+          callback: (response) => {
+            console.log('response', response)
+            handleSendOTP();
+          },
+          "expired-callback": () => {},
         },
-        validationSchema: LoginSchema,
-        onSubmit: async(values) => {
-            handleSendOTP(values)
-        }
-    })
-
-    const handleChange = (e) => {
-        const { name, value } = e.target
-        setValues({
-            ...values, [name] : value
-        })
+        auth
+      );
     }
+  }
+
+  const handleSendOTP = () => {
+    onCaptchVerify();
+    const appVerifier = window.recaptchaVerifier;
+    signInWithPhoneNumber(auth, "+91"+values.phoneNumber, appVerifier)
+      .then((confirmationResult) => {
+        window.confirmationResult = confirmationResult;
+        console.log("OTP Sent", confirmationResult
+          
+        );
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
+
+  const { values, errors, handleSubmit, setValues } = useFormik({
+    initialValues: {
+      phoneNumber: null,
+      keepSignIn: false,
+    },
+    validationSchema: LoginSchema,
+    onSubmit: async (values) => {
+      handleSendOTP(values);
+    },
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
+    });
+  };
 
   return (
     <div className="w-full h-full grid grid-cols-12">
@@ -56,10 +68,11 @@ const Login = () => {
         <img className="w-4/5" src={logo} alt="logo" />
       </div>
       <div className="col-span-12 md:col-span-5">
-        <Form handleSubmit={handleSubmit} className="w-full flex-col md:max-w-[480px] mx-auto p-4 md:p-8 h-full flex justify-center">
-          <h1 className="font-medium text-2xl mb-1 text-text">
-            Welcome Back!
-          </h1>
+        <Form
+          handleSubmit={handleSubmit}
+          className="w-full flex-col md:max-w-[480px] mx-auto p-4 md:p-8 h-full flex justify-center"
+        >
+          <h1 className="font-medium text-2xl mb-1 text-text">Welcome Back!</h1>
           <p className="text-base font-normal text-text-secondary mb-8">
             Discover fun, colorful outfits that let kids be kids!
           </p>
@@ -69,16 +82,28 @@ const Login = () => {
             type="number"
             label="Phone Number"
             error={errors.phoneNumber}
-            {...{name: 'phoneNumber', value: values.phoneNumber, handleChange}}
+            {...{
+              name: "phoneNumber",
+              value: values.phoneNumber,
+              handleChange,
+            }}
           />
-           <CustomCheckBox {...{name: 'keepSignIn', checked: values.keepSignIn, handleChange}}
+          <CustomCheckBox
+            {...{
+              name: "keepSignIn",
+              checked: values.keepSignIn,
+              handleChange,
+            }}
             className="mt-4 mb-8"
             label="Keep me signed in"
           />
-           <Button className="!w-full mb-1 !bg-pink !border-pink hover:border-yellow hover:bg-yellow transition-all duration-300 " type="submit">
+          <Button
+            className="!w-full mb-1 !bg-pink !border-pink hover:border-yellow hover:bg-yellow transition-all duration-300 "
+            type="submit"
+          >
             <span>Get OTP</span>
           </Button>
-          <div id='sign-in-button'></div>
+          <div id="recaptcha-container"></div>
         </Form>
       </div>
     </div>
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,31 +1,29 @@
 import { initializeApp } from 'firebase/app'
 import { getAuth, signInWithPhoneNumber, RecaptchaVerifier } from "firebase/auth";
 
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDLzDWKrSq2yEHDazMMbI22yVd2_Gt_d2Y",
+//     authDomain: "halfchoice-61cbf.firebaseapp.com",
+//     projectId: "halfchoice-61cbf",
+//     storageBucket: "halfchoice-61cbf.appspot.com",
+//     messagingSenderId: "11170268887",
+//     appId: "1:11170268887:web:51836859029993ecb69292",
+//     measurementId: "G-TP1XSB6PWB"
+// };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDLzDWKrSq2yEHDazMMbI22yVd2_Gt_d2Y",
-    authDomain: "halfchoice-61cbf.firebaseapp.com",
-    projectId: "halfchoice-61cbf",
-    storageBucket: "halfchoice-61cbf.appspot.com",
-    messagingSenderId: "11170268887",
-    appId: "1:11170268887:web:51836859029993ecb69292",
-    measurementId: "G-TP1XSB6PWB"
-};
+    apiKey: "AIzaSyCm9jnBYSweZYmlelDg8g38U1qYkDfAjss",
+    authDomain: "newsly-164bb.firebaseapp.com",
+    databaseURL: "https://newsly-164bb-default-rtdb.firebaseio.com",
+    projectId: "newsly-164bb",
+    storageBucket: "newsly-164bb.appspot.com",
+    messagingSenderId: "913260717277",
+    appId: "1:913260717277:web:6c733f70d337d647c8e734",
+    measurementId: "G-38YSEW8SNT"
+  };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 
-const signInPhoneNumber = (data,verifier) => {
-    signInWithPhoneNumber(data, verifier)
-    .then((confirmationResult)=>{
-        window.confirmationResult = confirmationResult;
-        console.log('OTP has been send');
-        return true;
-    })
-    .catch((error) => {
-        console.log('error :', error?.message)
-        return false;
-    })
-}
-
-export { auth, RecaptchaVerifier, signInPhoneNumber };
+export { auth, RecaptchaVerifier, signInWithPhoneNumber };
