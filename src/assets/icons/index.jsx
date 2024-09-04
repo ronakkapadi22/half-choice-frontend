287837815b06f438e22b5bcffad59ae3f6935d0f
Ronak Kapadi <ronakkapadi22@gmail.com>
2024-09-04 23:24:27 +0530

    Update footer

--- src/assets/icons/index.jsx
+++ src/assets/icons/index.jsx
@@ -4,6 +4,12 @@ import { HiBars3, HiShoppingCart  } from "react-icons/hi2";
 import { HiUserAdd, HiOutlineSearch, HiChevronDown } from "react-icons/hi";
 import { IoIosClose } from "react-icons/io";
 import { FaUserCircle } from "react-icons/fa";
+import { IoLocationOutline, IoMailOutline } from "react-icons/io5";
+import { TbWorld } from "react-icons/tb";
+import { BiPhoneCall } from "react-icons/bi";
+import { FiFacebook, FiYoutube } from "react-icons/fi";
+import { FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 
 export const ICONS = {
@@ -15,7 +21,15 @@ export const ICONS = {
     MENU: HiBars3,
     CHEVRON_DOWN: HiChevronDown,
     CLOSE: IoIosClose,
-    USER: FaUserCircle
+    USER: FaUserCircle,
+    TWITTER: FaXTwitter,
+    INSTAGRAM: FaInstagram,
+    FACEBOOK: FiFacebook,
+    YOUTUBE: FiYoutube,
+    PHONE: BiPhoneCall,
+    LOCATION: IoLocationOutline,
+    MAIL: IoMailOutline,
+    WORLD: TbWorld
 }
 
 
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,39 +1,30 @@
 import React from "react";
-import logo from "../../assets/images/logo.png";
-import { IoLocationOutline, IoMailOutline } from "react-icons/io5";
-import { TbWorld } from "react-icons/tb";
-import { BiPhoneCall } from "react-icons/bi";
-import { FiFacebook, FiYoutube } from "react-icons/fi";
-import { FaInstagram } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
+import logo_transparent from "../../assets/images/logo_transparent.svg";
 import Button from "../../shared/button";
 import { Link } from "react-router-dom";
+import { PAGES } from "../../assets/utils/urls";
+import { ICONS } from "../../assets/icons";
+import moment from "moment";
 
 const Footer = () => {
   return (
-    <footer
-      className="p-3 text-white md:p-20 bg-[#8DC63F]"
-      // style={{ clipPath: "circle(64.5% at 50% 100%)" }}
-    >
-      {/* Logo Positioned Between Sections */}
-      <div className="flex justify-center mx-auto mb-10">
-        <div className="p-4 bg-white rounded-full">
-          <Link to="/">
+    <footer className="p-3 text-white md:p-20 bg-green">
+      <div className="container px-0 mx-auto md:px-4 max-w-7xl flex justify-start items-center mb-8">
+        <div className="p-4 rounded-full">
+          <Link to={PAGES.HOME.path}>
             <img
-              src={logo} // replace with the actual logo path
+              src={logo_transparent}
               alt="HalfChoice Logo"
               className="h-16 w-fit"
             />
           </Link>
         </div>
       </div>
-
-      <div className="container px-0 mx-auto md:px-4 max-w-[1200px]">
+      <div className="container px-0 mx-auto md:px-4 max-w-7xl">
         <div className="flex flex-col justify-between w-full border border-t-0 md:flex-row border-x-0 pb-[20px]">
-          {/* Footer left Section */}
           <div className="flex flex-col">
             <div className="flex items-start gap-1 p-3">
-              <IoLocationOutline className="text-2xl" />
+              <ICONS.LOCATION className="text-2xl" />
               <address className="pl-1 text-base not-italic">
                 Anand IIyf, Vaishnodevi to Zundal circle, Tragad,
                 <br />
@@ -43,15 +34,15 @@ const Footer = () => {
               </address>
             </div>
             <div className="flex items-start gap-1 p-3">
-              <IoMailOutline className="text-2xl " />
+              <ICONS.MAIL className="text-2xl " />
               <p className="pl-1 text-base">Email: info@halfchoice.in</p>
             </div>
             <div className="flex items-start gap-1 p-3">
-              <TbWorld className="text-2xl " />
+              <ICONS.WORLD className="text-2xl " />
               <p className="pl-1 text-base">Website: www.halfchoice.in</p>
             </div>
             <div className="flex items-start gap-1 p-3">
-              <BiPhoneCall className="text-2xl " />
+              <ICONS.PHONE className="text-2xl " />
               <p className="pl-1 text-base">Phone: +91 81606 78824</p>
             </div>
           </div>
@@ -59,29 +50,29 @@ const Footer = () => {
           {/* Footer right Section */}
           <div className="px-6 pt-2">
             <div className="relative">
-              <form className="flex items-center">
+              <div className="flex items-center">
                 <input
                   type="email"
                   placeholder="Enter your email"
                   className="w-full px-4 py-3 text-black rounded-[30px] md:w-auto focus:outline-none border-none md:min-w-[340px] min-w-[240px]"
                 />
-                <Button className="ulinaBTN right-[39px] !rounded-[30px] ">
+                <Button className="!ml-2 !rounded-[30px] ">
                   <span>Submit</span>
                 </Button>
-              </form>
+              </div>
             </div>
             <div className="flex md:justify-end mr-[39px] mt-[60px] gap-3">
-              <div className="border rounded-[30px] p-3 cursor-pointer text-3xl bg-white text-[#84CC16]">
-                <FiFacebook />
+              <div className="border rounded-[30px] p-3 cursor-pointer text-3xl bg-white text-green">
+                <ICONS.FACEBOOK />
               </div>
-              <div className="border rounded-[30px] p-3 cursor-pointer text-3xl bg-white text-[#84CC16]">
-                <FaInstagram />
+              <div className="border rounded-[30px] p-3 cursor-pointer text-3xl bg-white text-green">
+                <ICONS.INSTAGRAM />
               </div>
-              <div className="border rounded-[30px] p-3 cursor-pointer text-3xl bg-white text-[#84CC16]">
-                <FaXTwitter />
+              <div className="border rounded-[30px] p-3 cursor-pointer text-3xl bg-white text-green">
+                <ICONS.TWITTER />
               </div>
-              <div className="border rounded-[30px] p-3 cursor-pointer text-3xl bg-white text-[#84CC16]">
-                <FiYoutube />
+              <div className="border rounded-[30px] p-3 cursor-pointer text-3xl bg-white text-green">
+                <ICONS.YOUTUBE />
               </div>
             </div>
           </div>
@@ -91,35 +82,27 @@ const Footer = () => {
         <div className="flex flex-col-reverse justify-between gap-2 mt-8 md:items-center md:flex-row">
           <div className="pl-2 md:pl-0 md:text-left">
             <p className="text-base">
-              Copyright © 2023 HalfChoice. All Rights Reserved.
+              Copyright © {moment().year()} HalfChoice. All Rights Reserved.
             </p>
           </div>
-          <div className="flex flex-col justify-center text-base md:justify-start md:flex-row">
-            <Link to="/" className="text-white hover:text-[#ff2189] px-3">
+          <div className="flex justify-start text-base md:justify-start md:flex-row">
+            <Link to={PAGES.HOME.path} className="text-white hover:text-pink px-3">
               Home
             </Link>
-            <Link to="/about" className="text-white hover:text-[#ff2189] px-3">
+            <Link to={PAGES.ABOUT.path} className="text-white hover:text-pink px-3">
               About us
             </Link>
             <Link
-              to="/contact"
-              className="text-white hover:text-[#ff2189] px-3"
+              to={PAGES.CONTACT_US.path}
+              className="text-white hover:text-pink px-3"
             >
               contact us
             </Link>
-            <Link
-              to="/features"
-              className="text-white hover:text-[#ff2189] px-3"
-            >
-              Features
-            </Link>
-            <Link to="/blog" className="text-white hover:text-[#ff2189] px-3">
+            <Link to={PAGES.HOME.path} className="text-white hover:text-pink px-3">
               Blogs
             </Link>
           </div>
         </div>
-
-        {/* Footer Links */}
       </div>
     </footer>
   );
